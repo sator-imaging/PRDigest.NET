62790692210a8b3647c7ee3432e4e4693923b012
prozolic <42107886+prozolic@users.noreply.github.com>
2025-12-22 17:30:03 +0900

    Add Google tag

--- src/PRDigest.NET/HtmlGenereator.cs
+++ src/PRDigest.NET/HtmlGenereator.cs
@@ -70,6 +70,15 @@ internal static class HtmlGenereator
 <!DOCTYPE html>
 <html lang="ja">
 <head>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-34XNJ13EZY"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'G-34XNJ13EZY');
+  </script>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>{{title}}</title>
